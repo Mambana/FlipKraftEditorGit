785816247bfa9a4dc7d0edf27b3151dac5cc3383
Samy Aitouaki <samy.aitouakli@gmail.com>
2019-01-10 16:45:27 +0000

    [ADD] ApiButton and HttpRequest
    ApiButton os a test Script for button, allowing them to use the API script
    HttpRequest handles connection to the API

---
+++ Assets/Scripts/ApiButton.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Text;
+
+public class ApiButton : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameObject.GetComponent<Button>().onClick.AddListener(click);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void click()
+    {
+        HttpRequest www = gameObject.GetComponent<HttpRequest>();
+        //Dictionary<string, string> test = new Dictionary<string, string>();
+
+        //test["testKey"] = "testValue";
+        string test = "test";
+        www.Put("https://ptsv2.com/t/y6qvl-1547042899/post", Encoding.ASCII.GetBytes(test), Debug.Log);
+    }
+}
