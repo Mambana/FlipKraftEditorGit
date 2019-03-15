2b89c885412ce7d04a462a97734d7a854c036226
Etienne <fernandezetiennej@outlook.fr>
2019-03-15 09:41:02 +0100

    refonte ui

--- Assets/Scripts/LoginController.cs
+++ Assets/Scripts/LoginController.cs
@@ -1,13 +1,30 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using TMPro;
 public class LoginController : BasicController
 {
+    [SerializeField]
+    GameObject uriInput;
     // Start is called before the first frame update
     void Start()
     {
-        
+        string uri = Application.absoluteURL;
+        int slashIdx = uri.IndexOf('/');
+        if (slashIdx == -1)
+            {
+                uriInput.GetComponent<TMP_InputField>().text = uri;
+                return;
+            }
+        string uriPart = uri.Substring(0, slashIdx);
+        int lastIdx = uriPart.IndexOf('/');
+        if (lastIdx == -1)
+        {
+            uriInput.GetComponent<TMP_InputField>().text = uri;
+            return;
+        }
+        string apiUri = uri.Substring(slashIdx + 2, lastIdx);
+        uriInput.GetComponent<TMP_InputField>().text = apiUri;
     }
 
     // Update is called once per frame
